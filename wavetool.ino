208fbc1ad714a50b69e0e5c17868210c37bf5cb4
Robert Beenen <robert@foresense.org>
2018-06-15 02:18:05 +0200

    working, almost function complete

--- wavetool.ino
+++ wavetool.ino
@@ -13,167 +13,170 @@ by Robert Beenen
 
 	*/
 
-#include <avr/pgmspace.h>
-#include <avr/interrupt.h>
+//#include <avr/pgmspace.h>
+//#include <avr/interrupt.h>
+
+//TCNT1 = 65386;
 
 #include "SPI.h"
 #include "kas-button.h"
-#include "voctave.h"
+#include "voltoctave.h"
 #include "colundi.h"
 #include "evolver.h"
 
-// hardware pins
-#define MOD1_PIN 		A1
-#define MOD2_PIN 		A2
-#define TRIGGER_PIN 	4
-#define BUTTON1_PIN 	2
-#define BUTTON2_PIN 	9
-#define LED1_PIN 		3
-#define LED2_PIN 		5
+#define MOD1_PIN 	A1
+#define MOD2_PIN 	A2
+#define TRIGGER_PIN 4
+#define BUTTON1_PIN 2
+#define BUTTON2_PIN 9
+#define LED1_PIN 	3
+#define LED2_PIN 	5
+#define OUTPUT_PIN	10
 
-// hardware variables
-word mod1;
-word mod2;
+#define FRAMESIZE	128
+#define INTERPOINTS	8		// number of points to interpolate over
+
+// input
+uint16_t mod1;
+uint16_t mod2;
+Button trigger;
 Button button1;
 Button button2;
-word output;
 
-// internal variables
-word pitch;
-word pitch_phase;
-word wave;
-word wave_offset;
-byte speed = 1;
-byte speed_offset = 2;
-byte phase = 0;
-word sample;
-word sample_i;
-bool tuning = false;
-bool full_cycle = true;
+// internal
+bool colundi = false;
+bool interpolation = true;
+uint16_t offset;
+uint8_t ratio;
+uint16_t offset_p;		// only updates at phase 0
+uint8_t ratio_p;		// so waveforms don't sound garbled when changed
+uint8_t phase = 0;
+uint8_t step = 1;
 
-void setup()
+// output
+uint16_t timer1_preload;
+uint16_t sample;
+
+void setup() 
 {
-
-	Serial.begin(9600);
-	Serial.print("WAVETOOL DEBUG OUT - F_CPU: ");
-	Serial.println(F_CPU);
+	// Serial.begin(9600);
+	// Serial.println('WAVE DEBUG');
 
 	cli();
-	TCCR1A = 0;
-	TCCR1B = 1;
-	TIMSK1 |= (1 << TOIE1);
+	TCCR1A = 0b00000000;		// timer1 flags
+	TCCR1B = 0b00000001;		// no prescaling
+	TIMSK1 |= (1 << TOIE1);		// interrupt enable
 	TCNT1 = 0;
 	sei();
 
-	pinMode(10, OUTPUT);
-	
+	pinMode(OUTPUT_PIN, OUTPUT);
+
 	SPI.begin();
 	SPI.setBitOrder(MSBFIRST);
 
-	//pinMode(TRIGGER_PIN, INPUT_PULLUP);
+	pinMode(TRIGGER_PIN, INPUT_PULLUP);
 	pinMode(BUTTON1_PIN, INPUT_PULLUP);
 	pinMode(BUTTON2_PIN, INPUT_PULLUP);
+	trigger.pin = TRIGGER_PIN;
 	button1.pin = BUTTON1_PIN;
 	button2.pin = BUTTON2_PIN;
-	
-	digitalWrite(LED1_PIN, tuning);
-	digitalWrite(LED2_PIN, full_cycle);
 
-	TCNT1 = 65386;
+	digitalWrite(LED1_PIN, colundi);
+	digitalWrite(LED2_PIN, interpolation);
 }
-
-//int linearMap(int input, int size_in, int size_out);
 
 void loop()
 {
 	mod1 = analogRead(MOD1_PIN);
-	mod2 = analogRead(MOD2_PIN);
+	mod2 = (analogRead(MOD2_PIN) * 3) >> 2;		// map 1024 to 768:
+												// 96 waveforms, and 3 bits interpolation
+	ratio = mod2 & 0b111;
+	offset = (mod2 >> 3) * FRAMESIZE;			
 
-	if(tuning)
+	if(updateButton(&trigger))
 	{
-		mod1 = (mod1 * 10) >> 8;	// integer math: map 0-1023 -> 0-39
-		switch(mod1) {
+		if(trigger.state)
+		{
+		}
+	}
+	if(updateButton(&button1))
+	{
+		if(button1.state)
+		{
+			colundi = !colundi;
+		}
+		digitalWrite(LED1_PIN, colundi);
+	}
+	if(updateButton(&button2))
+	{
+		if(button2.state)
+		{
+			interpolation = !interpolation;
+		}
+		digitalWrite(LED2_PIN, interpolation);
+	}
+
+	if(colundi)
+	{
+		mod1 = (mod1 * 10) >> 8;	// map 0-1023 to 0-39
+		switch(mod1)
+		{
 			case 35:
 			case 36:
 			case 37:
-				speed = 4;
+				step = 2;
 				break;
 			case 38:
-				speed = 16;
+				step = 8;
 				break;
 			case 39:
-				speed = 64;
+				step = 32;
 				break;
 			default:
-				speed = 2;	// full 128 bytes
-				break;
+				step = 1;
 		}
-		pitch = pgm_read_word_near(colundi + mod1);
+		timer1_preload = pgm_read_word_near(colundiMap + mod1);
 	}
 	else
 	{
-		speed = 2;
-		pitch = pgm_read_word_near(voctave + mod1);
-	}
-	
-	mod2 = round((mod2 << 6) / 682);	// integer math: mapping 0-1023 -> 0-96
-	wave = mod2 << 7;
-	wave_i = (mod2 + 1) << 7;
-
-	if(updateButton(&button1)) {
-		if(button1.state) {
-			tuning = !tuning;
-		}
-		digitalWrite(LED1_PIN, tuning);
-	}
-	if(updateButton(&button2)) {
-		if(button2.state) {
-			full_cycle = !full_cycle;
-			phase = 0;
-		}
-		digitalWrite(LED2_PIN, full_cycle);
+		step = 1;
+		timer1_preload = pgm_read_word_near(voltoctaveMap + mod1);
 	}
 }
 
 ISR(TIMER1_OVF_vect)
 {
-	TCNT1 = pitch_phase;
-	pitch_phase = pitch;
-
-	// speed needs to change when it is aligned with phase at 0
-	if(speed > speed_offset && !(phase % speed))
-	{
-		speed_offset = speed;
-	}
+	TCNT1 = timer1_preload;
 	
-	if(full_cycle && !phase)
-	{
-		wave_offset = wave;
-		sample = pgm_read_word(&wavetable[wave_offset + (phase >> 1)]);
-	}
-	else if (!full_cycle)
-	{
-		speed_offset = speed;
-		wave_offset = wave;
-		sample = pgm_read_word
-	}
-	
-	/*
-	if(interpolate)
-	{
-		sample_i = sample & 0xfff;
-		sample_i *= wave_interpolation ^ 0b111;
-		sample_i = 
+	// step needs to change when it is aligned with phase at 0
+	//if(step > step && !(phase % step))
+	//{
+	//	step_out = step;
+	//}
 
-		sample += pgm_read_word(&wavetable[wave_offset + (phase >> 1) + 1]);
-		sample >> 1;
+	if(!phase)
+	{
+		offset_p = offset;
+		ratio_p = ratio;
 	}
-	*/
+
+	if(interpolation)
+	{
+		// interpolate between 2 waveforms in 3 bits
+		sample = pgm_read_word(&wavetable[offset_p + phase]) * (INTERPOINTS - ratio_p);
+		sample += pgm_read_word(&wavetable[offset_p + FRAMESIZE + phase]) * ratio_p;
+		sample >>= 3;
+	}
+	else
+	{
+		sample = pgm_read_word(&wavetable[offset_p + phase]);
+	}
 
 	PORTB &= 0b11111011;
 	SPI.transfer(highByte(sample) | 0x30);
 	SPI.transfer(lowByte(sample));
 	PORTB |= 0b00000100;
 
-	phase += speed_offset;
+	phase += step;
+	phase &= 0b01111111;
 }
