89b505d7d72539ff418526123e1169a65417df94
Robert Beenen <robert@foresense.org>
2018-06-23 14:35:49 +0200

    basic working version

--- wavetool.ino
+++ wavetool.ino
@@ -1,44 +1,36 @@
-/*	
+/*
 
 WAVETOOL
-Robert Beenen
-foresense.org
+by Robert Beenen
 
-	Mod 1:		Pitch
-	Mod 2:		Wavetable
-	Trigger:	
-	Button 1:	
-	Button 2:
-	LED 1:		
-	LED 2:
+    Mod 1:    Pitch
+    Mod 2:    Wavetable
+	Button 1: -
+    Button 2: Interpolation
+	Trigger:  -
 
 	*/
 
+//#define DEBUG
+
 #include "SPI.h"
-#include "kas-button.h"
-#include "voltoctave.h"
-#include "colundi.h"
+#include "tuning.h"
 #include "evolver.h"
 
-#define MOD1_PIN 	A1
-#define MOD2_PIN 	A2
-#define TRIGGER_PIN 4
-#define BUTTON1_PIN 2
-#define BUTTON2_PIN 9
-#define LED1_PIN 	3
-#define LED2_PIN 	5
-#define OUTPUT_PIN	10
+#define MOD1_PIN 		A1
+#define MOD2_PIN 		A2
+#define TRIGGER_PIN 	4
+#define BUTTON1_PIN 	2
+#define BUTTON2_PIN 	9
+#define LED1_PIN 		3
+#define LED2_PIN 		5
+#define OUTPUT_PIN		10
 
 // input
-uint16_t mod1;
-uint16_t mod2;
-Button trigger;
-Button button1;
-Button button2;
+uint16_t mod1, mod2;
+uint8_t trigger, button1, button2;
 
 // internal
-bool colundi = false;
-bool interpolation = true;
 uint16_t offset, offset_p;
 uint8_t ratio, ratio_p;
 uint8_t phase = 0;
@@ -48,11 +40,10 @@ uint8_t step = 1;
 uint16_t timer1_preload;
 uint16_t sample;
 
+bool interpolation = true;
+
 void setup() 
 {
-	// Serial.begin(9600);
-	// Serial.println('WAVE DEBUG');
-
 	cli();
 	TCCR1A = 0b00000000;		// timer1 flags
 	TCCR1B = 0b00000001;		// no prescaling
@@ -64,88 +55,32 @@ void setup()
 
 	SPI.begin();
 	SPI.setBitOrder(MSBFIRST);
-
-	pinMode(TRIGGER_PIN, INPUT_PULLUP);
-	pinMode(BUTTON1_PIN, INPUT_PULLUP);
-	pinMode(BUTTON2_PIN, INPUT_PULLUP);
-	trigger.pin = TRIGGER_PIN;
-	button1.pin = BUTTON1_PIN;
-	button2.pin = BUTTON2_PIN;
-
-	digitalWrite(LED1_PIN, colundi);
-	digitalWrite(LED2_PIN, interpolation);
 }
 
 void loop()
 {
 	mod1 = analogRead(MOD1_PIN);
-	mod2 = (analogRead(MOD2_PIN) * 3) >> 2;		// map 1024 to 768:
-												// 96 waveforms, and 3 bits interpolation
+	mod2 = (analogRead(MOD2_PIN) * 3) >> 2;		// 3/4 range: 96 waveforms
+
 	ratio = mod2 & 0b111;
-	offset = (mod2 >> 3) * 0x7F;
+	offset = (mod2 >> 3) * 0x80;
 
-	if(updateButton(&trigger))
-	{
-		if(trigger.state)
-		{
-		}
-	}
-	if(updateButton(&button1))
-	{
-		if(button1.state)
-		{
-			colundi = !colundi;
-		}
-		digitalWrite(LED1_PIN, colundi);
-	}
-	if(updateButton(&button2))
-	{
-		if(button2.state)
-		{
-			interpolation = !interpolation;
-		}
-		digitalWrite(LED2_PIN, interpolation);
-	}
-
-	if(colundi)
-	{
-		mod1 = (mod1 * 10) >> 8;	// map 0-1023 to 0-39
-		switch(mod1)
-		{
-			case 35:
-			case 36:
-			case 37:
-				step = 2;
-				break;
-			case 38:
-				step = 8;
-				break;
-			case 39:
-				step = 32;
-				break;
-			default:
-				step = 1;
-		}
-		timer1_preload = pgm_read_word_near(colundiMap + mod1);
-	}
-	else
-	{
-		step = 1;
-		timer1_preload = pgm_read_word_near(voltoctaveMap + mod1);
-	}
+	timer1_preload = pgm_read_word(&tuning_map[mod1]);
 }
 
 ISR(TIMER1_OVF_vect)
 {
+	
 	/*
-	the timer1 interrupt (this function) is retriggered at the end of
-	the timer1 counter. set this counter at the start of the interrupt
-	so it will be used to set the update frequency, and the pitch.
+	
+	the timer1 overflow interrupt (TIMER1_OVF_vect) is retriggered at the end of the
+	trigger1 counter (TCNT1)
+	
 	*/
 
 	TCNT1 = timer1_preload;
 
-	// cycle length is 7 bits, 8th bit is overflow
+	// cycle length is 7 bits, the 8th bit signals overflow
 	if(phase & 0b10000000)
 	{
 		offset_p = offset;
@@ -156,8 +91,9 @@ ISR(TIMER1_OVF_vect)
 	if(interpolation)
 	{
 		// interpolate between 2 waveforms in 3 bits
-		sample = pgm_read_word(&wavetable[offset_p + phase]) * (8 - ratio_p);
-		sample += pgm_read_word(&wavetable[offset_p + 0x7F + phase]) * ratio_p;
+		// the (uint8_t) cast saves cycles. avoid 16 bit math on an 8 bit cpu.
+		sample = pgm_read_word(&wavetable[offset_p + phase]) * (uint8_t)(8 - ratio_p); 
+		sample += pgm_read_word(&wavetable[offset_p + phase + 0x80]) * ratio_p;
 		sample >>= 3;
 	}
 	else
@@ -165,10 +101,11 @@ ISR(TIMER1_OVF_vect)
 		sample = pgm_read_word(&wavetable[offset_p + phase]);
 	}
 
-	PORTB &= 0b11111011;
-	SPI.transfer(highByte(sample) | 0x30);	// 0x30 = tell the DAC to do something
-	SPI.transfer(lowByte(sample));
-	PORTB |= 0b00000100;
+										// 12 bit DAC
+	PORTB &= 0b11111011;				// open SPI for writing
+	SPI.transfer((sample >> 8) | 0x30);	// sample ms nibble + DAC flag '0b0011'
+	SPI.transfer(sample & 0x00FF);		// sample ls byte
+	PORTB |= 0b00000100;				// close SPI
 
 	phase += step;
 }
